9f6665ee83e7347906e7a036e67f684f3a169cac
Marcus Jun <mjun@calstate.edu>
2018-04-21 21:05:57 +0000

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,17 @@
+class Project
+
+  attr_accessor :backers, :title
+
+  def initialize (title)
+    @title=title
+    @backers=[]
+  end
+
+  def add_backer(backer)
+    @backers<<backer
+    backer.back_project(self) unless backer.backed_projects.include?(self)
+    #Backer.back_project(self)
+  end
+
+
+end
